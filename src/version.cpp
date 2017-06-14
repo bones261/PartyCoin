e498192c1ca6d51a86f6661f9f6f8cb1fd831cef
bones261 <bones261@comcast.net>
2017-06-13 23:45:47 -0600

    added checkpoint. Updated version number

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("PartyCoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "32a928e"
+#    define GIT_COMMIT_ID "d5909f2"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
